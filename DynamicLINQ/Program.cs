5a5bd4c07fe7008ddabb0f43edf874c66ebe0504
Wayne <seriawei@gmail.com>
2014-07-01 16:37:32 +0800

    ds

--- DynamicLINQ/Program.cs
+++ DynamicLINQ/Program.cs
@@ -12,21 +12,41 @@ namespace DynamicLINQ
     {
         static void Main(string[] args)
         {
-            List<B> values = new List<B>
+            for (int i = 0; i < 6; i++)
             {
-               new B(){A="All"},
-               new B(){A="Al"},
-               new B(){A="C"}
-            };
+                int total = 1;
+                for (int k = 0; k < i + 1; k++)
+                {
+                    total *= 10;
+                }
+                List<A> values = new List<A>();
+                for (int j = 0; j < total; j++)
+                {
+                    values.Add(new A { PB = new B { Filter = "Filter_" + j } });
+                }
+                Console.WriteLine("Data:{0};", total);
+                DateTime start = DateTime.Now;
+                var resu = values.AsQueryable().Where("PB.Filter.Contains(\"Filter_2\") && PB.Filter.Contains(\"Filter_20\")");
+                Console.Write("Dynamic:{0};\t", resu.Count());
+                Console.Write("Timespan:{0};", (DateTime.Now - start).TotalMilliseconds);
 
-            var resu = values.AsQueryable().Where("A.Contains(\"Al\")");
-            var resuC = values.AsQueryable().Where("A.Contains(@0)","C");
+                start = DateTime.Now;
+                var resu2 = values.Where(m => m.PB.Filter.Contains("Filter_2") && m.PB.Filter.Contains("Filter_20"));
+                Console.Write("Normal:{0};\t", resu2.Count());
+                Console.Write("Timespan:{0};", (DateTime.Now - start).TotalMilliseconds);
+                Console.WriteLine("\r\n-----------------------------------------------------");
+            }
 
+            Console.ReadKey();
         }
     }
 
+    public class A
+    {
+        public B PB { get; set; }
+    }
     public class B
     {
-        public string A { get; set; }
+        public string Filter { get; set; }
     }
 }
